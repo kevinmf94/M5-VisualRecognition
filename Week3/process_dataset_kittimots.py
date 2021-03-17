a366102f6f9b72eb704aafb23e3819ce638bf77b
Kevin Martín <kevinmf94@gmail.com>
2021-03-17 20:04:26 +0100

    Fix dataset generation

--- Week3/process_dataset_kittimots.py
+++ Week3/process_dataset_kittimots.py
@@ -1,17 +1,18 @@
 import os
 import pickle
+import sys
 
 from detectron2.structures import BoxMode
 from pycocotools import mask
 import pandas as pd
 
 KITTIMOTS_DATASET = '/home/mcv/datasets/KITTI-MOTS/'
-KITTIMOTS_TRAIN = KITTIMOTS_DATASET + "training/image_02/"
-KITTIMOTS_TRAIN_INSTANCES = KITTIMOTS_DATASET + "instances_txt/"
-TRAIN = ["0000.txt", "0001.txt", "0002.txt", "0003.txt", "0004.txt",
-         "0005.txt", "0006.txt", "0007.txt", "0008.txt", "0009.txt"]
-VALIDATION = ["0010.txt", "0011.txt"]
-TEST = ["0012.txt", "0013.txt", "0014.txt", "0015.txt", "0016.txt", "0017.txt", "0018.txt", "0019.txt", "0020.txt"]
+KITTIMOTS_IMAGES = KITTIMOTS_DATASET + "training/image_02/"
+KITTIMOTS_IMG_INSTANCES = KITTIMOTS_DATASET + "instances_txt/"
+TRAIN = ["0000", "0001", "0002", "0003", "0004",
+         "0005", "0006", "0007", "0008", "0009"]
+VALIDATION = ["0010", "0011"]
+TEST = ["0012", "0013", "0014", "0015", "0016", "0017", "0018", "0019", "0020"]
 
 
 def rle_to_bbox(row):
@@ -34,29 +35,36 @@ def annotations_to_dict(annotations):
     return objs
 
 
-def frame_to_record(sequence, frame, annotations):
+def frame_to_record(sequence, frame, annotations, size):
     return {
-        "file_name": os.path.join(KITTIMOTS_TRAIN, sequence) + ("/%06d.png" % frame),
+        "file_name": os.path.join(KITTIMOTS_IMAGES, sequence) + ("/%06d.png" % frame),
         "image_id": "%s_%06d" % (sequence, frame),
-        "height": annotations.Height.values[0],
-        "width": annotations.Width.values[0],
+        "height": size[0],
+        "width": size[1],
         "annotations": annotations_to_dict(annotations)
     }
 
 
-def generate_dict(files):
+def generate_dict(sequences):
     dataset_dict = []
-    for file in files:
-        data = pd.read_csv(KITTIMOTS_TRAIN_INSTANCES + file, delimiter=" ",
+
+    for sequence in sequences:
+        images = os.listdir(os.path.join(KITTIMOTS_IMAGES, sequence))
+        frames = len(images)
+        images.sort()
+
+        data = pd.read_csv(KITTIMOTS_IMG_INSTANCES + sequence + ".txt", delimiter=" ",
                            names=["Frame", "Id", "Class", "Height", "Width", "RLE", "BBox"])
         data = data.apply(lambda row: rle_to_bbox(row), axis=1)
-        data = data[(data.Class == 1) | (data.Class == 2)]
-        sequence = file.split(".")[0]
-
+        height = data.Height[0]
+        width = data.Width[0]
         print("Processing sequence %s" % sequence, flush=True)
 
-        for frame in data['Frame'].unique():
-            record = frame_to_record(sequence, frame, data[data.Frame == frame])
+        for frame in range(frames):
+            print("Processing frame %s" % frame, flush=True, end="\r")
+            annotations = data[data.Frame == frame]
+            annotations = annotations[(annotations.Class == 1) | (annotations.Class == 2)]
+            record = frame_to_record(sequence, frame, annotations, (height, width))
             dataset_dict.append(record)
 
     return dataset_dict
@@ -67,19 +75,19 @@ if __name__ == '__main__':
     print("Generating KITTI-MOTS", flush=True)
 
     train_records = generate_dict(TRAIN)
-    print("Train %d" % len(train_records))
+    print("\nTrain %d" % len(train_records))
     with open('kittimots_train.dat', 'wb') as f:
         pickle.dump(train_records, f)
         print("Generated kittimots_train.dat", flush=True)
 
     val_records = generate_dict(VALIDATION)
-    print("Val %d" % len(val_records))
+    print("\nVal %d" % len(val_records))
     with open('kittimots_val.dat', 'wb') as f:
         pickle.dump(val_records, f)
         print("Generated kittimots_val.dat", flush=True)
 
     test_records = generate_dict(TEST)
-    print("Test %d" % len(test_records))
+    print("\nTest %d" % len(test_records))
     with open('kittimots_test.dat', 'wb') as f:
         pickle.dump(test_records, f)
         print("Generated kittimots_test.dat", flush=True)
--- Week3/process_dataset_mots.py
+++ Week3/process_dataset_mots.py
@@ -6,10 +6,10 @@ from pycocotools import mask
 import pandas as pd
 
 MOTS_DATASET = '/home/mcv/datasets/MOTSChallenge/'
-MOTS_TRAIN = MOTS_DATASET + "train/"
+MOTS_TRAIN_IMG = MOTS_DATASET + "train/images/"
 MOTS_TRAIN_INSTANCES = MOTS_DATASET + "train/instances_txt/"
-TRAIN = ["0002.txt", "0005.txt", "0009.txt"]
-VALIDATION = ["0011.txt"]
+TRAIN = ["0002", "0005", "0009"]
+VALIDATION = ["0011"]
 
 
 def rle_to_bbox(row):
@@ -35,28 +35,36 @@ def annotations_to_dict(annotations):
     return objs
 
 
-def frame_to_record(sequence, frame, annotations):
+def frame_to_record(sequence, frame, annotations, size):
     return {
-        "file_name": os.path.join(MOTS_TRAIN, "images/", sequence) + ("/%06d.jpg" % frame),
+        "file_name": os.path.join(MOTS_TRAIN_IMG, sequence) + ("/%06d.jpg" % frame),
         "image_id": "%s_%06d" % (sequence, frame),
-        "height": annotations.Height.values[0],
-        "width": annotations.Width.values[0],
+        "height": size[0],
+        "width": size[1],
         "annotations": annotations_to_dict(annotations)
     }
 
 
-def generate_dict(files):
+def generate_dict(sequences):
     dataset_dict = []
-    for file in files:
-        data = pd.read_csv(MOTS_TRAIN_INSTANCES + file, delimiter=" ",
+
+    for sequence in sequences:
+        images = os.listdir(os.path.join(MOTS_TRAIN_IMG, sequence))
+        frames = len(images)
+        images.sort()
+
+        data = pd.read_csv(MOTS_TRAIN_INSTANCES + sequence + ".txt", delimiter=" ",
                            names=["Frame", "Id", "Class", "Height", "Width", "RLE", "BBox"])
         data = data.apply(lambda row: rle_to_bbox(row), axis=1)
-        sequence = file.split(".")[0]
+        height = data.Height[0]
+        width = data.Width[0]
 
         print("Processing sequence %s" % sequence, flush=True)
-
-        for frame in data['Frame'].unique():
-            record = frame_to_record(sequence, frame, data[data.Frame == frame])
+        for frame in range(frames):
+            print("Processing frame %s" % frame, flush=True, end="\r")
+            annotations = data[data.Frame == frame]
+            annotations = annotations[(annotations.Class == 2)]
+            record = frame_to_record(sequence, frame, annotations, (height, width))
             dataset_dict.append(record)
 
     return dataset_dict
@@ -67,13 +75,13 @@ if __name__ == '__main__':
     print("Generating MOTSChallenge", flush=True)
 
     train_records = generate_dict(TRAIN)
-    print("Train %d" % len(train_records))
+    print("\nTrain %d" % len(train_records))
     with open('mots_train.dat', 'wb') as f:
         pickle.dump(train_records, f)
         print("Generated mots_train.dat", flush=True)
 
     val_records = generate_dict(VALIDATION)
-    print("Val %d" % len(val_records))
+    print("\nVal %d" % len(val_records))
     with open('mots_validation.dat', 'wb') as f:
         pickle.dump(val_records, f)
         print("Generated mots_validation.dat", flush=True)
